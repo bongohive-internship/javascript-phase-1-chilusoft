63f134ec149db006c3e1393800cc4e30d6312e40
Chilusoft <mukukachilu@gmail.com>
2019-08-17 12:17:03 +0200

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,8 +1,8 @@
 // you solution should be here
 var birthYear = 1996;
 var futureYear = 2019;
-var age1 = birthYear - futureYear;
-var age2 = (birthYear - futureYear) - 1;
+var age1 = futureYear - birthYear;
+var age2 = (futureYear - birthYear) - 1;
 
 console.log("My two possible ages are: " + age1 + " and " + age2); //part 1 ends here
 
@@ -10,7 +10,7 @@ var currentAge = 23;
 var maxAge = 80;
 var estAmt = 5;
 
-console.log("You will need " + (estAmt *365*(maxAge - currentAge)) + "to last you until the ripe old age of " + maxAge + ".");
+console.log("You will need " + (estAmt *365*(maxAge - currentAge)) + " to last you until the ripe old age of " + maxAge + ".");
 
 
 
